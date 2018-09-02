fb16c9959ea1fa30358bbd2844212235c3028576
erjicles <erjicles@gmail.com>
2018-09-02 00:33:54 -0500

    Added initial items to populate database

--- prepopulate_database.py
+++ prepopulate_database.py
@@ -11,16 +11,34 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 # Add initial categories
-categories = []
-categories.append(Category(name="Soccer"))
-categories.append(Category(name="Basketball"))
-categories.append(Category(name="Baseball"))
-categories.append(Category(name="Frisbee"))
-categories.append(Category(name="Snowboarding"))
-categories.append(Category(name="Rock Climbing"))
-categories.append(Category(name="Foosball"))
-categories.append(Category(name="Skating"))
-categories.append(Category(name="Hockey"))
-for category in categories:
-    session.add(category)
-session.commit()
+if session.query(Category).count() == 0:
+    categories = []
+    categories.append(Category(name="Soccer"))
+    categories.append(Category(name="Basketball"))
+    categories.append(Category(name="Baseball"))
+    categories.append(Category(name="Frisbee"))
+    categories.append(Category(name="Snowboarding"))
+    categories.append(Category(name="Rock Climbing"))
+    categories.append(Category(name="Foosball"))
+    categories.append(Category(name="Skating"))
+    categories.append(Category(name="Hockey"))
+    for category in categories:
+        session.add(category)
+    session.commit()
+
+
+# Add initial items
+if session.query(Item).count() == 0:
+    items = []
+    items.append(Item(name="Soccer ball", category_id=1))
+    items.append(Item(name="Hoop", category_id=2))
+    items.append(Item(name="Bat", category_id=3))
+    items.append(Item(name="Disc", category_id=4))
+    items.append(Item(name="Board", category_id=5))
+    items.append(Item(name="Ropes", category_id=6))
+    items.append(Item(name="Table", category_id=7))
+    items.append(Item(name="Skates", category_id=8))
+    items.append(Item(name="Stick", category_id=9))
+    for item in items:
+        session.add(item)
+    session.commit()
