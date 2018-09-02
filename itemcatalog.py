7fb91153eecd4746dcc3192e510a795a1ff2f3ac
erjicles <erjicles@gmail.com>
2018-09-01 20:14:33 -0500

    Updated to select all categories for landing page requests

--- itemcatalog.py
+++ itemcatalog.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, User, Category, Item
+import json
+from flask import make_response
+import requests
 
 app = Flask(__name__)
 
 APPLICATION_NAME = "Item Catalog Application"
 
+# Connect to Database and create database session
+engine = create_engine('sqlite:///itemcatalog.db',
+    connect_args={'check_same_thread': False})
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 # Landing page - show all categories and recent items
 @app.route('/')
 @app.route('/category/')
 def showCategories():
-    return render_template('categories.html')
+    categories = session.query(Category).order_by(asc(Category.name)).all()
+    return render_template('categories.html', categories=categories)
 
 
 if __name__ == '__main__':
